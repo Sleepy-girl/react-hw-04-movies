e5cf2b52d62ae31656c7e8dd7736f24f78d4f3c8
nyuta <pillowforneedles@gmail.com>
2020-08-19 19:54:43 +0300

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+  Link,
+  NavLink,
+} from 'react-router-dom';
 import HomePage from '../views/HomePage';
 import MoviesPage from '../views/MoviesPage';
 import MovieDetailsPage from '../views/MovieDetailsPage';
 import Cast from '../views/Cast';
 import Reviews from '../views/Reviews';
-import NotFound from '../views/NotFound';
+// import NotFound from '../views/NotFound';
 
 export class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/movies" component={MoviesPage} />
-          <Route exact path="/movies/:movieId" component={MovieDetailsPage} />
-          <Route exact path="/movies/:movieId/cast" component={Cast} />
-          <Route path="/movies/:movieId/reviews" component={Reviews} />
-          <Route component={NotFound} />
-        </Switch>
+        <>
+          <ul className="Navigation-list">
+            <li>
+              <NavLink
+                exact
+                to="/"
+                className="Navigation-link"
+                activeClassName="Navigation-link-active"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                exact
+                to="/movies"
+                className="Navigation-link"
+                activeClassName="Navigation-link-active"
+              >
+                Movies
+              </NavLink>
+            </li>
+          </ul>
+
+          <hr />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/movies" component={MoviesPage} />
+            <Route exact path="/movies/:movieId" component={MovieDetailsPage} />
+            <Route path="/movies/:movieId/cast" component={Cast} />
+            <Route path="/movies/:movieId/reviews" component={Reviews} />
+            <Redirect to="/" />
+            {/* <Route component={NotFound} /> */}
+          </Switch>
+        </>
       </BrowserRouter>
     );
   }
